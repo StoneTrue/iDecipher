daf2806ae285dd1678541e6dc71d85f9ed0b3464
Stone True <raustin333i@gmail.com>
2017-01-29 19:17:52 -0500

    Updated comments for new structure

--- id.c
+++ id.c
@@ -1,3 +1,30 @@
+//  Main menu
+// 1 - enter a cipher text from std in (e.g. user types ANDJDBFKSJVN)
+// 2 - read a cipher text from a .txt or other file
+// 3 - analyze a cipher text
+// 4 - help
+// 5 - exit
+//
+//  user chooses option - only can choose 1 - 5, anything else 
+//  results in being asked to enter 1 - 5
+//  1 - enter cipher from stdin & save as user defined file 
+//  2 - read cioher text from user defined file ans save as user 
+//  defined file
+//  3 - analyze file chosen; default is most recent
+//  analysis options are:
+//  3.1 - develop key from user defined features, eg number of
+//  characters per key element
+//  3.2 -  perform frequency analysis
+//  3.3 -  enter a plain key manually
+//  3.4 -  calculate possible keys from frquency analysis
+//  3.5 -  calculate possible keys from user entered key
+//  3.6 -  do a Caeser shift
+//  3.7 -  wheels 
+//  
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
